b1c89f8ca64f908f2ea0e7aa547b3e8b9574fdbf
JuKu <kuenzel.justin@t-online.de>
2017-12-07 02:25:37 +0100

    added new main class.

---
+++ desktop/src/main/java/com/jukusoft/mmo/desktop/DesktopLauncher.java
@@ -0,0 +1,47 @@
+package com.jukusoft.mmo.desktop;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.jukusoft.mmo.game.Game;
+import com.jukusoft.mmo.utils.ReportUtils;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * Starts the application for the desktop-based builds.
+ *
+ * @author Leeks & Dragons
+ */
+public class DesktopLauncher {
+
+    protected static int WIDTH = 1280;
+    protected static int HEIGHT = 720;
+
+    /**
+     * start method for game application
+     */
+    public static void main(String[] args) {
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setTitle("2D MMORPG");
+        config.setWindowedMode(WIDTH, HEIGHT);
+        //config.setWindowIcon("./data/icon/app_icon.png");
+        config.setResizable(true);
+
+        //https://gamedev.stackexchange.com/questions/109047/how-to-close-an-app-correctly-on-desktop
+        //config.forceExit = false;
+
+        try {
+            // start game
+            new Lwjgl3Application(new Game(), config);
+        } catch (Exception e) {
+            e.printStackTrace();
+
+            //send exception to server
+            ReportUtils.sendExceptionToServer(e);
+
+            System.exit(-1);
+        }
+    }
+
+}
---
+++ game/src/main/java/com/jukusoft/mmo/game/Game.java
@@ -0,0 +1,6 @@
+package com.jukusoft.mmo.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+public class Game extends ApplicationAdapter {
+}
