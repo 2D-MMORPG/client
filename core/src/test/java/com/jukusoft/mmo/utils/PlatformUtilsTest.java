9944c12cc3cf1988695e54444fa9643a3ce2129c
JuKu <kuenzel.justin@t-online.de>
2017-12-10 21:06:26 +0100

    added PlatformUtils.

---
+++ core/src/test/java/com/jukusoft/mmo/utils/PlatformUtilsTest.java
@@ -0,0 +1,12 @@
+package com.jukusoft.mmo.utils;
+
+import org.junit.Test;
+
+public class PlatformUtilsTest {
+
+    @Test
+    public void testGetType () {
+        PlatformUtils.getType();
+    }
+
+}
