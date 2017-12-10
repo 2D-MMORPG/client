fee96f84176b5671c68c446ace315464adba8a80
JuKu <kuenzel.justin@t-online.de>
2017-12-10 15:42:59 +0100

    added some junit tests.

--- core/src/test/java/com/jukusoft/mmo/utils/LogUtilsTest.java
+++ core/src/test/java/com/jukusoft/mmo/utils/LogUtilsTest.java
@@ -17,11 +17,23 @@ public class LogUtilsTest {
     public void testCreateLogDirIfAbsent () {
         AppUtils.setAppName("junit-test");
 
+        if (new File(LogUtils.getLogPath()).exists()) {
+            new File(LogUtils.getLogPath()).delete();
+        }
+
         LogUtils.createLogDirIfAbsent();
 
         //check, if directory exists
         assertEquals(true, new File(LogUtils.getLogPath("junit-test")).exists());
 
+    }
+
+    @Test
+    public void testEndsWithSlash () {
+        AppUtils.setAppName("junit-test");
+
+        assertEquals(true, LogUtils.getLogPath().endsWith("/"));
+        assertEquals(true, LogUtils.getLogPath("junit-test2").endsWith("/"));
     }
 
 }
