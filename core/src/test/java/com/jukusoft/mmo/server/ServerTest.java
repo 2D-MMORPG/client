a98a8d5892ab20ce9d6d319145b33b2de77d10c3
JuKu <kuenzel.justin@t-online.de>
2017-12-11 16:01:50 +0100

    added some junit tests.

--- core/src/test/java/com/jukusoft/mmo/server/ServerTest.java
+++ core/src/test/java/com/jukusoft/mmo/server/ServerTest.java
@@ -50,4 +50,94 @@ public class ServerTest {
         assertEquals("none", server.getImageURL());
     }
 
+    @Test (expected = IllegalArgumentException.class)
+    public void testNotExistentKeyLoad1 () {
+        Server server = new Server();
+
+        //create new example json object
+        JsonObject json = new JsonObject();
+        json.put("description", "test-description");
+        json.put("channel", "stable");
+        json.put("proxy_ip", "127.0.0.1");
+        json.put("proxy_port", 2600);
+        json.put("image_url", "none");
+
+        server.load(json);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void testNotExistentKeyLoad2 () {
+        Server server = new Server();
+
+        //create new example json object
+        JsonObject json = new JsonObject();
+        json.put("name", "test-name");
+        json.put("channel", "stable");
+        json.put("proxy_ip", "127.0.0.1");
+        json.put("proxy_port", 2600);
+        json.put("image_url", "none");
+
+        server.load(json);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void testNotExistentKeyLoad3 () {
+        Server server = new Server();
+
+        //create new example json object
+        JsonObject json = new JsonObject();
+        json.put("name", "test-name");
+        json.put("description", "test-description");
+        json.put("proxy_ip", "127.0.0.1");
+        json.put("proxy_port", 2600);
+        json.put("image_url", "none");
+
+        server.load(json);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void testNotExistentKeyLoad4 () {
+        Server server = new Server();
+
+        //create new example json object
+        JsonObject json = new JsonObject();
+        json.put("name", "test-name");
+        json.put("description", "test-description");
+        json.put("channel", "stable");
+        json.put("proxy_port", 2600);
+        json.put("image_url", "none");
+
+        server.load(json);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void testNotExistentKeyLoad5 () {
+        Server server = new Server();
+
+        //create new example json object
+        JsonObject json = new JsonObject();
+        json.put("name", "test-name");
+        json.put("description", "test-description");
+        json.put("channel", "stable");
+        json.put("proxy_ip", "127.0.0.1");
+        json.put("image_url", "none");
+
+        server.load(json);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void testNotExistentKeyLoad6 () {
+        Server server = new Server();
+
+        //create new example json object
+        JsonObject json = new JsonObject();
+        json.put("name", "test-name");
+        json.put("description", "test-description");
+        json.put("channel", "stable");
+        json.put("proxy_ip", "127.0.0.1");
+        json.put("proxy_port", 2600);
+
+        server.load(json);
+    }
+
 }
