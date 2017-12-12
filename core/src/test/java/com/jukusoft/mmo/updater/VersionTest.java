56b0314040b6f4f5e09eda4ebe4e85afb4ebc45b
JuKu <kuenzel.justin@t-online.de>
2017-12-12 11:20:22 +0100

    added junit tests for Version.

---
+++ core/src/test/java/com/jukusoft/mmo/updater/VersionTest.java
@@ -0,0 +1,138 @@
+package com.jukusoft.mmo.updater;
+
+import io.vertx.core.json.JsonObject;
+import org.junit.Test;
+
+public class VersionTest {
+
+    @Test
+    public void testConstructor () {
+        new Version();
+    }
+
+    @Test (expected = NullPointerException.class)
+    public void testLoadNull () {
+        Version version = new Version();
+        version.load(null);
+    }
+
+    @Test
+    public void testLoad () {
+        //create new example json object
+        JsonObject json = new JsonObject();
+        json.put("version", "1.0.0-alpha");
+        json.put("full_version", "1.0.0 Pre-Alpha");
+        json.put("build_number", 1);
+        json.put("build_date", "12.12.2017");
+        json.put("build_time", "11.28");
+        json.put("update_channel_url", "http://example.tld");
+
+        //create new data model
+        Version version = new Version();
+
+        //load data model from json object
+        version.load(json);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void testLoadInvalideKeys () {
+        //create new example json object
+        JsonObject json = new JsonObject();
+        json.put("full_version", "1.0.0 Pre-Alpha");
+        json.put("build_number", 1);
+        json.put("build_date", "12.12.2017");
+        json.put("build_time", "11.28");
+        json.put("update_channel_url", "http://example.tld");
+
+        //create new data model
+        Version version = new Version();
+
+        //load data model from json object
+        version.load(json);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void testLoadInvalideKeys1 () {
+        //create new example json object
+        JsonObject json = new JsonObject();
+        json.put("version", "1.0.0-alpha");
+        json.put("build_number", 1);
+        json.put("build_date", "12.12.2017");
+        json.put("build_time", "11.28");
+        json.put("update_channel_url", "http://example.tld");
+
+        //create new data model
+        Version version = new Version();
+
+        //load data model from json object
+        version.load(json);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void testLoadInvalideKeys2 () {
+        //create new example json object
+        JsonObject json = new JsonObject();
+        json.put("version", "1.0.0-alpha");
+        json.put("full_version", "1.0.0 Pre-Alpha");
+        json.put("build_date", "12.12.2017");
+        json.put("build_time", "11.28");
+        json.put("update_channel_url", "http://example.tld");
+
+        //create new data model
+        Version version = new Version();
+
+        //load data model from json object
+        version.load(json);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void testLoadInvalideKeys3 () {
+        //create new example json object
+        JsonObject json = new JsonObject();
+        json.put("version", "1.0.0-alpha");
+        json.put("full_version", "1.0.0 Pre-Alpha");
+        json.put("build_number", 1);
+        json.put("build_time", "11.28");
+        json.put("update_channel_url", "http://example.tld");
+
+        //create new data model
+        Version version = new Version();
+
+        //load data model from json object
+        version.load(json);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void testLoadInvalideKeys4 () {
+        //create new example json object
+        JsonObject json = new JsonObject();
+        json.put("version", "1.0.0-alpha");
+        json.put("full_version", "1.0.0 Pre-Alpha");
+        json.put("build_number", 1);
+        json.put("build_date", "12.12.2017");
+        json.put("update_channel_url", "http://example.tld");
+
+        //create new data model
+        Version version = new Version();
+
+        //load data model from json object
+        version.load(json);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void testLoadInvalideKeys5 () {
+        //create new example json object
+        JsonObject json = new JsonObject();
+        json.put("version", "1.0.0-alpha");
+        json.put("full_version", "1.0.0 Pre-Alpha");
+        json.put("build_number", 1);
+        json.put("build_date", "12.12.2017");
+        json.put("build_time", "11.28");
+
+        //create new data model
+        Version version = new Version();
+
+        //load data model from json object
+        version.load(json);
+    }
+}
