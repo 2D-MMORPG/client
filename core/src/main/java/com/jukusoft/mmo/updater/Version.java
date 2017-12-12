e2579ae66c052e96f3377c5501a59e0376dc640c
JuKu <kuenzel.justin@t-online.de>
2017-12-12 10:49:42 +0100

    added Version.

---
+++ core/src/main/java/com/jukusoft/mmo/updater/Version.java
@@ -0,0 +1,47 @@
+package com.jukusoft.mmo.updater;
+
+import io.vertx.core.json.JsonObject;
+
+/**
+* data model to get current version
+*/
+public class Version {
+
+    protected String version = "";
+    protected String fullVersion = "";
+    protected int buildNumber = -1;
+
+    //build date & time
+    protected String buildDate = "";
+    protected String buildTime = "";
+
+    //url to update channels on server
+    protected String updateURL = "";
+
+    /**
+    * default constructor
+    */
+    public Version () {
+        //
+    }
+
+    /**
+    * load from json object
+     *
+     * @param json json object
+    */
+    public void load (JsonObject json) {
+        if (!json.containsKey("version") || !json.containsKey("full_version") || !json.containsKey("build_number") || !json.containsKey("build_date") || !json.containsKey("build_time") || !json.containsKey("update_channel_url")) {
+            throw new IllegalArgumentException("json object doesnt contains all required keys.");
+        }
+
+        //get values
+        this.version = json.getString("version");
+        this.fullVersion = json.getString("full_version");
+        this.buildNumber = json.getInteger("build_number");
+        this.buildDate = json.getString("build_date");
+        this.buildTime = json.getString("build_time");
+        this.updateURL = json.getString("update_channel_url");
+    }
+
+}
