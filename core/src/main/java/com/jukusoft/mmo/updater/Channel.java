32e19882871f7a9c487ccbd247b6daea2b3cdf3d
JuKu <kuenzel.justin@t-online.de>
2017-12-12 12:01:33 +0100

    added new class Channel.

---
+++ core/src/main/java/com/jukusoft/mmo/updater/Channel.java
@@ -0,0 +1,65 @@
+package com.jukusoft.mmo.updater;
+
+import io.vertx.core.json.JsonObject;
+
+/**
+* Data model for update channel
+*/
+public class Channel {
+
+    //data values
+    protected String name = "";
+    protected String title = "";
+
+    //flag, if channel is activated and can be used
+    protected boolean activated = false;
+
+    //flag, if channel is public, so every user can login to server with normal credentials
+    protected boolean publicChannel = false;
+
+    //url to file hashes
+    protected String updateURL = "";
+
+    /**
+    * default constructor
+    */
+    public Channel () {
+        //
+    }
+
+    /**
+    * load channel from json object
+    */
+    public void load (JsonObject json) {
+        if (json == null) {
+            throw new NullPointerException("json cannot be null.");
+        }
+
+        //get values
+        this.name = json.getString("name");
+        this.title = json.getString("title");
+        this.activated = json.getBoolean("activated");
+        this.publicChannel = json.getBoolean("public");
+        this.updateURL = json.getString("update_url");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public boolean isActivated() {
+        return activated;
+    }
+
+    public boolean isPublic() {
+        return publicChannel;
+    }
+
+    public String getUpdateURL() {
+        return updateURL;
+    }
+}
---
+++ core/src/test/java/com/jukusoft/mmo/updater/ChannelTest.java
@@ -0,0 +1,52 @@
+package com.jukusoft.mmo.updater;
+
+import io.vertx.core.json.JsonObject;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ChannelTest {
+
+    @Test
+    public void testConstructor () {
+        //
+    }
+
+    @Test (expected = NullPointerException.class)
+    public void testLoadNull () {
+        Channel channel = new Channel();
+        channel.load(null);
+    }
+
+    @Test
+    public void testLoad () {
+        //create new instance of channel
+        Channel channel = new Channel();
+
+        //create example json object
+        JsonObject json = new JsonObject();
+        json.put("name", "test-name");
+        json.put("title", "test-title");
+        json.put("activated", true);
+        json.put("public", true);
+        json.put("update_url", "http://example.tld");
+
+        //load channel from json object
+        channel.load(json);
+
+        //check object values
+        assertEquals("test-name", channel.name);
+        assertEquals("test-title", channel.title);
+        assertEquals(true, channel.activated);
+        assertEquals(true, channel.publicChannel);
+        assertEquals("http://example.tld", channel.updateURL);
+
+        //test getter methods
+        assertEquals("test-name", channel.getName());
+        assertEquals("test-title", channel.getTitle());
+        assertEquals(true, channel.isActivated());
+        assertEquals(true, channel.isPublic());
+        assertEquals("http://example.tld", channel.getUpdateURL());
+    }
+
+}
