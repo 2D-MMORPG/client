2d0a9749096f211abf256ce843dcadca41a2cc31
JuKu <kuenzel.justin@t-online.de>
2017-12-12 19:57:09 +0100

    added some files.

--- core/src/test/java/com/jukusoft/mmo/updater/UpdaterTest.java
+++ core/src/test/java/com/jukusoft/mmo/updater/UpdaterTest.java
@@ -1,10 +1,24 @@
 package com.jukusoft.mmo.updater;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 
 public class UpdaterTest {
+
+    @BeforeClass
+    public static void beforeClass () {
+        //
+    }
+
+    @AfterClass
+    public static void afterClass () throws IOException {
+        Files.delete(new File("files.json").toPath());
+    }
 
     @Test
     public void testConstructor () {
@@ -15,6 +29,12 @@ public class UpdaterTest {
     public void testLoadOwnVersion () throws IOException {
         Updater updater = new Updater();
         updater.loadOwnVersion("../updater/");
+    }
+
+    @Test
+    public void testPrepareFileHashesNotExistentDir () throws Exception {
+        Updater updater = new Updater();
+        updater.prepareFileHashes("../junit-tests/updater/");
     }
 
     @Test
