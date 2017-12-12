d04942feed37634b6ae5481f8fe8e7a69dbfdf62
JuKu <kuenzel.justin@t-online.de>
2017-12-12 19:36:30 +0100

    improved software quality for sonarqube.

--- core/src/main/java/com/jukusoft/mmo/utils/WebUtils.java
+++ core/src/main/java/com/jukusoft/mmo/utils/WebUtils.java
@@ -3,7 +3,6 @@ package com.jukusoft.mmo.utils;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 
