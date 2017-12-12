1c9cda802dc9d1a5c21d9a49dfa49fdb11307938
JuKu <kuenzel.justin@t-online.de>
2017-12-12 22:35:58 +0100

    added interface FXMLController.

---
+++ launcher/src/main/java/com/jukusoft/mmo/launcher/javafx/FXMLController.java
@@ -0,0 +1,15 @@
+package com.jukusoft.mmo.launcher.javafx;
+
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+/**
+ * Created by Justin on 30.05.2017.
+ */
+public interface FXMLController extends Runnable, Initializable {
+
+    public void init(Stage stage, Scene scene, Pane pane);
+
+}
