a456b6ea32a8f247097c8589c61d71e795c8b0d5
JuKu <kuenzel.justin@t-online.de>
2017-12-26 23:21:00 +0100

    changed network manager, added support to load network configuration.

--- core/src/main/java/com/jukusoft/mmo/network/impl/DefaultNetworkManager.java
+++ core/src/main/java/com/jukusoft/mmo/network/impl/DefaultNetworkManager.java
@@ -1,11 +1,18 @@
 package com.jukusoft.mmo.network.impl;
 
 import com.jukusoft.mmo.network.NetworkManager;
+import com.jukusoft.mmo.network.config.NetConfig;
 import com.jukusoft.mmo.network.message.MessageReceiver;
 import com.jukusoft.mmo.network.traffic.TrafficCounter;
+import com.jukusoft.mmo.utils.ReportUtils;
 import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
 import io.vertx.core.buffer.Buffer;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class DefaultNetworkManager implements NetworkManager<Buffer> {
 
@@ -14,6 +21,9 @@ public class DefaultNetworkManager implements NetworkManager<Buffer> {
 
     //vertx instance
     protected Vertx vertx = null;
+
+    //network config
+    protected NetConfig config = null;
 
     //traffic counter
     protected TrafficCounter counter = null;
@@ -30,6 +40,9 @@ public class DefaultNetworkManager implements NetworkManager<Buffer> {
         //create new traffic counter
     }
 
+    public void load (String configFile) throws IOException {
+        this.config = new NetConfig(configFile);
+    }
 
     @Override
     public void send(Buffer msg, PROTOCOL protocol) {
@@ -67,6 +80,20 @@ public class DefaultNetworkManager implements NetworkManager<Buffer> {
     public static DefaultNetworkManager getManagerInstance () {
         if (instance == null) {
             instance = new DefaultNetworkManager();
+
+            //load configuration
+            try {
+                if (!new File("./data/config/network.cfg").exists()) {
+                    //its an junit test
+                    instance.load("../data/config/junit-network.cfg");
+                } else {
+                    instance.load("./data/config/junit-network.cfg");
+                }
+            } catch (IOException e) {
+                ReportUtils.sendExceptionToServer(e);
+
+                Logger.getAnonymousLogger().log(Level.SEVERE, "Exception occurred while loading network configuration file: ", e);
+            }
         }
 
         return instance;
