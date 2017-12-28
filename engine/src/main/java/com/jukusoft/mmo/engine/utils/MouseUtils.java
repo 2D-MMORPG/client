b6f3d41f97d890645b49f2d6db6ac476f9357651
JuKu <kuenzel.justin@t-online.de>
2017-12-28 13:20:49 +0100

    improved software quality for sonarqube.

--- engine/src/main/java/com/jukusoft/mmo/engine/utils/MouseUtils.java
+++ engine/src/main/java/com/jukusoft/mmo/engine/utils/MouseUtils.java
@@ -16,14 +16,18 @@ public class MouseUtils {
 
     public static float getMouseX(float x, CameraHelper camera) {
         return (x + camera.getX()) * 1
-                / camera.getZoom();/* - (viewportWidth / 2) */
+                / camera.getZoom();
     }
 
     public static float getMouseY(float y, CameraHelper camera) {
-        // y = camera.getOriginalCamera().viewportHeight - y;
         return (y + camera.getY()) * 1 / camera.getZoom();
     }
 
+    /**
+    * get mouse position dependent from camera
+     *
+     * @deprecated use getMousePositionWithCamera(CameraHelper camera) instead
+    */
     @Deprecated
     public static Vector3 getMousePositionWithCamera(Camera camera) {
         tmpVector.set(Gdx.input.getX(), Gdx.input.getY(), 0);
@@ -69,17 +73,6 @@ public class MouseUtils {
 
         return tmpVector2;
     }
-
-    /*public static float getCorrectWindowY () {
-        //first, check if fullscreen mode
-        if (Gdx.graphics.isFullscreen()) {
-            return Gdx.input.getY();
-        } else {
-            //TODO: check, if it is an decorated window
-
-            return Gdx.input.getY() + 31;
-        }
-    }*/
 
     public static float correctDecoredMousePositionY (float y) {
         float a = (((float) Gdx.graphics.getHeight() - 31f) / (float) Gdx.graphics.getHeight());
