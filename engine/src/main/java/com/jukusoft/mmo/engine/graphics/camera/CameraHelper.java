1808890b6e22db3b8fe58e8df5ec076d00ea2fc4
JuKu <kuenzel.justin@t-online.de>
2017-12-28 20:38:07 +0100

    improved software quality for sonarqube.

--- engine/src/main/java/com/jukusoft/mmo/engine/graphics/camera/CameraHelper.java
+++ engine/src/main/java/com/jukusoft/mmo/engine/graphics/camera/CameraHelper.java
@@ -220,25 +220,7 @@ public class CameraHelper implements ModificationFinishedListener {
         } else if (mode == CameraMode.FIXED_CAMERA) {
             //dont move camera
         } else if (mode == CameraMode.MOUSE_CAMERA) {
-            //get current mouse positon
-            float mouseX = Gdx.input.getX();
-            float mouseY = Gdx.input.getY();
-
-            if (mouseX < 0 + this.mouseScrollBorder) {
-                //scroll left
-                this.x = this.x - this.mouseScrollSpeed;
-            } else if (mouseX > getViewportWidth() - this.mouseScrollBorder) {
-                //scroll right
-                this.x = this.x + this.mouseScrollSpeed;
-            }
-
-            if (mouseY < 0 + this.mouseScrollBorder) {
-                //scroll down
-                this.y = this.y - this.mouseScrollSpeed;
-            } else if (mouseY > getViewportHeight() - this.mouseScrollBorder) {
-                //scroll up
-                this.y = this.y + this.mouseScrollSpeed;
-            }
+            this.updateMouseScrollCamera();
         } else if (mode == CameraMode.SCROLL_CAMERA_WITH_MAX_DISTANCE) {
             //TODO: add code here
         } else {
@@ -247,6 +229,46 @@ public class CameraHelper implements ModificationFinishedListener {
         }
 
         //check, if new position is in bounds, if not correct position
+        this.correctBounds(lastX, lastY);
+
+        //reset temporary camera position
+        this.tempCameraParams.reset(getX(), getY(), getZoom());
+
+        //update modifications first
+        for (CameraModification mod : this.activeModifications) {
+            mod.onUpdate(time, this.tempCameraParams, this);
+        }
+
+        //sync camera helper position to libGDX camera
+        this.syncPosToCamera();
+
+        //update libGDX camera
+        this.camera.update();
+    }
+
+    protected void updateMouseScrollCamera () {
+        //get current mouse positon
+        float mouseX = Gdx.input.getX();
+        float mouseY = Gdx.input.getY();
+
+        if (mouseX < 0 + this.mouseScrollBorder) {
+            //scroll left
+            this.x = this.x - this.mouseScrollSpeed;
+        } else if (mouseX > getViewportWidth() - this.mouseScrollBorder) {
+            //scroll right
+            this.x = this.x + this.mouseScrollSpeed;
+        }
+
+        if (mouseY < 0 + this.mouseScrollBorder) {
+            //scroll down
+            this.y = this.y - this.mouseScrollSpeed;
+        } else if (mouseY > getViewportHeight() - this.mouseScrollBorder) {
+            //scroll up
+            this.y = this.y + this.mouseScrollSpeed;
+        }
+    }
+
+    protected void correctBounds (float lastX, float lastY) {
         if (this.mode != CameraMode.FIXED_CAMERA) {
             //check, if camera can scroll
             float deltaX = this.x - lastX;
@@ -286,20 +308,6 @@ public class CameraHelper implements ModificationFinishedListener {
                 }
             }
         }
-
-        //reset temporary camera position
-        this.tempCameraParams.reset(getX(), getY(), getZoom());
-
-        //update modifications first
-        for (CameraModification mod : this.activeModifications) {
-            mod.onUpdate(time, this.tempCameraParams, this);
-        }
-
-        //sync camera helper position to libGDX camera
-        this.syncPosToCamera();
-
-        //update libGDX camera
-        this.camera.update();
     }
 
     /**
