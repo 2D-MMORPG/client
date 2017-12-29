d2bd9cb26de24ec4484ecffb7f3d4a79b07c1ea9
JuKu <kuenzel.justin@t-online.de>
2017-12-30 00:07:58 +0100

    improved software quality for sonarqube.

--- engine/src/test/java/com/jukusoft/mmo/engine/utils/GameTimeTest.java
+++ engine/src/test/java/com/jukusoft/mmo/engine/utils/GameTimeTest.java
@@ -1,18 +1,87 @@
 package com.jukusoft.mmo.engine.utils;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Graphics;
+import com.badlogic.gdx.*;
+import com.badlogic.gdx.backends.headless.HeadlessApplication;
 import com.badlogic.gdx.graphics.Cursor;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.GL30;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.glutils.GLVersion;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class GameTimeTest {
+
+    protected static Application application = null;
+
+    @BeforeClass
+    public static void beforeClass () {
+        //http://manabreak.eu/java/2016/10/21/unittesting-libgdx.html
+
+        // Note that we don't need to implement any of the listener's methods
+        application = new HeadlessApplication(new ApplicationListener() {
+            @Override public void create() {}
+            @Override public void resize(int width, int height) {}
+            @Override public void render() {}
+            @Override public void pause() {}
+            @Override public void resume() {}
+            @Override public void dispose() {}
+        });
+
+        application.setApplicationLogger(new ApplicationLogger() {
+            @Override
+            public void log(String tag, String message) {
+                Logger.getAnonymousLogger().log(Level.SEVERE, tag, message);
+            }
+
+            @Override
+            public void log(String tag, String message, Throwable exception) {
+                Logger.getAnonymousLogger().log(Level.SEVERE, tag  + ": " + message, exception);
+            }
+
+            @Override
+            public void error(String tag, String message) {
+                Logger.getAnonymousLogger().log(Level.SEVERE, tag + ": " + message);
+            }
+
+            @Override
+            public void error(String tag, String message, Throwable exception) {
+                Logger.getAnonymousLogger().log(Level.SEVERE, tag + ": " + message);
+            }
+
+            @Override
+            public void debug(String tag, String message) {
+                Logger.getAnonymousLogger().log(Level.INFO, tag, message);
+                System.err.println("DEBUG [" + tag + "] " + message);
+            }
+
+            @Override
+            public void debug(String tag, String message, Throwable exception) {
+                Logger.getAnonymousLogger().log(Level.INFO, tag + ": " + message, exception);
+            }
+        });
+
+        // Use Mockito to mock the OpenGL methods since we are running headlessly
+        Gdx.gl20 = Mockito.mock(GL20.class);
+        Gdx.gl = Gdx.gl20;
+
+        Gdx.app = application;
+    }
+
+    @AfterClass
+    public static void afterClass () {
+        // Exit the application first
+        application.exit();
+        application = null;
+    }
 
     @Test
     public void testConstructor () {
@@ -266,6 +335,40 @@ public class GameTimeTest {
 
         //update
         gameTime.update(false);
+
+        assertEquals(true, currentTime <= gameTime.getTime());
+        assertEquals(true, System.currentTimeMillis() >= gameTime.getTime());
+    }
+
+    @Test
+    public void testUpdate3 () {
+        GameTime gameTime = new GameTime();
+
+        //check startup time
+        assertEquals(true, gameTime.getStartUpTime() <= System.currentTimeMillis());
+
+        long currentTime = System.currentTimeMillis();
+
+        //update
+        gameTime.update();
+
+        assertEquals(true, currentTime <= gameTime.getTime());
+        assertEquals(true, System.currentTimeMillis() >= gameTime.getTime());
+    }
+
+    @Test
+    public void testUpdate4 () {
+        GameTime gameTime = new GameTime();
+
+        //check startup time
+        assertEquals(true, gameTime.getStartUpTime() <= System.currentTimeMillis());
+
+        long currentTime = System.currentTimeMillis();
+
+        Gdx.graphics = null;
+
+        //update
+        gameTime.update();
 
         assertEquals(true, currentTime <= gameTime.getTime());
         assertEquals(true, System.currentTimeMillis() >= gameTime.getTime());
