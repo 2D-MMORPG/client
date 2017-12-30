d5f62fa6bba8f618e9197c46d12685b32a7f541c
JuKu <kuenzel.justin@t-online.de>
2017-12-30 19:04:24 +0100

    added some junit tests.

---
+++ engine/src/test/java/com/jukusoft/mmo/engine/service/impl/SpriteBatchServiceTest.java
@@ -0,0 +1,43 @@
+package com.jukusoft.mmo.engine.service.impl;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.jukusoft.mmo.engine.GameTest;
+import com.jukusoft.mmo.engine.service.ServiceManager;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class SpriteBatchServiceTest extends GameTest {
+
+    @Test
+    public void testConstructor () {
+        new SpriteBatchService();
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void testStartAndStop () {
+        ServiceManager serviceManager = new DefaultServiceManager();
+
+        SpriteBatchService service = new SpriteBatchService();
+        service.batch = Mockito.mock(SpriteBatch.class);
+        serviceManager.addService(service, SpriteBatchService.class);
+
+        serviceManager.removeService(SpriteBatchService.class);
+    }
+
+    @Test
+    public void testDraw () {
+        SpriteBatchService service = new SpriteBatchService();
+        service.batch = Mockito.mock(SpriteBatch.class);
+
+        service.draw();
+    }
+
+    @Test
+    public void testAfterDraw () {
+        SpriteBatchService service = new SpriteBatchService();
+        service.batch = Mockito.mock(SpriteBatch.class);
+
+        service.afterDraw();
+    }
+
+}
