ee6aa18a2a185de8c29ba6746b34f508bc10fb0f
JuKu <kuenzel.justin@t-online.de>
2017-12-31 18:21:36 +0100

    added interface for screen manager.

---
+++ engine/src/main/java/com/jukusoft/mmo/engine/graphics/screen/ScreenManager.java
@@ -0,0 +1,74 @@
+package com.jukusoft.mmo.engine.graphics.screen;
+
+import java.util.Collection;
+
+/**
+ * Created by Justin on 06.02.2017.
+ */
+public interface ScreenManager<T extends IScreen> {
+
+    /**
+     * add screen
+     *
+     * @param name name of screen
+     * @param screen instance of screen
+     */
+    public void addScreen(final String name, T screen);
+
+    /**
+     * remove screen
+     *
+     * @param name name of screen
+     */
+    public void removeScreen(final String name);
+
+    /**
+     * push screen
+     *
+     * @param name name of screen
+     */
+    public void push(final String name);
+
+    /**
+     * leave all active game states and enter an new one
+     *
+     * @param name name of new game state
+     */
+    public void leaveAllAndEnter(final String name);
+
+    /**
+     * pop screen
+     *
+     * @return instance of top screen
+     */
+    public T pop();
+
+    /**
+    * get screen by name
+     *
+     * @param name unique name of screen
+     *
+     * @return instance of screen
+    */
+    public T getScreenByName (final String name);
+
+    /**
+     * list all initialized screens
+     *
+     * @return list with all registered streams
+     */
+    public Collection<T> listScreens();
+
+    /**
+     * list all active screens
+     *
+     * @return list with all active screens
+     */
+    public Collection<T> listActiveScreens();
+
+    /**
+    * dispose screen manager with all screens
+    */
+    public void dispose();
+
+}
