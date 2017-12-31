2b12514ed86fcdc2c9b87d3827c0bd0df9e3fe39
JuKu <kuenzel.justin@t-online.de>
2017-12-31 18:35:00 +0100

    added some junit tests.

--- engine/src/test/java/com/jukusoft/mmo/engine/app/BaseAppTest.java
+++ engine/src/test/java/com/jukusoft/mmo/engine/app/BaseAppTest.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.headless.HeadlessApplication;
 import com.badlogic.gdx.graphics.GL20;
+import com.jukusoft.mmo.engine.service.IService;
 import com.jukusoft.mmo.engine.service.ServiceManager;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -199,6 +200,78 @@ public class BaseAppTest {
 
     @Test
     public void testRender2 () throws InterruptedException {
+        BaseApp app = new BaseApp() {
+            @Override
+            protected void createServices(ServiceManager serviceManager) {
+                created = true;
+            }
+
+            @Override
+            public int getFPS () {
+                return 50;
+            }
+        };
+
+        app.serviceManager = new ServiceManager() {
+            @Override
+            public <T extends IService> void addService(T service, Class<T> cls) {
+                //
+            }
+
+            @Override
+            public <T extends IService> void removeService(Class<T> cls) {
+                //
+            }
+
+            @Override
+            public <T extends IService> T getService(Class<T> cls) {
+                return null;
+            }
+
+            @Override
+            public <T extends IService> boolean existsService(Class<T> cls) {
+                return false;
+            }
+
+            @Override
+            public void processInput() {
+                //
+            }
+
+            @Override
+            public void update() {
+                //
+            }
+
+            @Override
+            public void draw() {
+                throw new RuntimeException("test exception");
+            }
+
+            @Override
+            public void afterDraw() {
+                //
+            }
+        };
+
+        // Note that we don't need to implement any of the listener's methods
+        application = new HeadlessApplication(app);
+
+        // Use Mockito to mock the OpenGL methods since we are running headlessly
+        Gdx.gl20 = Mockito.mock(GL20.class);
+        Gdx.gl = Gdx.gl20;
+
+        Thread.sleep(100);
+
+        app.lastFPS = 60;
+
+        Thread.sleep(100);
+
+        application.exit();
+    }
+
+    @Test
+    public void testRender3 () throws InterruptedException {
         BaseApp app = new BaseApp() {
             @Override
             protected void createServices(ServiceManager serviceManager) {
