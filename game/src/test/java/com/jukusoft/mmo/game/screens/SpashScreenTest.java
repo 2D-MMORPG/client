ef0c00ae32a9fa9f629bdd18a0a162aa0c3828c2
JuKu <kuenzel.justin@t-online.de>
2018-01-03 16:31:28 +0100

    added some junit tests.

---
+++ game/src/test/java/com/jukusoft/mmo/game/screens/SpashScreenTest.java
@@ -0,0 +1,38 @@
+package com.jukusoft.mmo.game.screens;
+
+import com.jukusoft.mmo.game.GameUnitTest;
+import org.junit.Test;
+
+public class SpashScreenTest extends GameUnitTest {
+
+    @Test
+    public void testConstructor () {
+        new SplashScreen();
+    }
+
+    @Test
+    public void testStartAndStop () {
+        SplashScreen screen = new SplashScreen();
+
+        screen.onStart();
+
+        screen.onResume();
+
+        screen.onPause();
+
+        screen.onStop();
+    }
+
+    @Test
+    public void testUpdate () {
+        SplashScreen screen = new SplashScreen();
+        screen.update();
+    }
+
+    @Test
+    public void testDraw () {
+        SplashScreen screen = new SplashScreen();
+        screen.draw();
+    }
+
+}
