f7fcf02f521887c79154d8c86ec9fdc98c19b665
JuKu <kuenzel.justin@t-online.de>
2018-01-29 02:08:01 +0100

    changed Poolable.

--- network-shared/src/main/java/com/jukusoft/mmo/pool/Poolable.java
+++ network-shared/src/main/java/com/jukusoft/mmo/pool/Poolable.java
@@ -1,5 +1,8 @@
 package com.jukusoft.mmo.pool;
 
+/**
+* interface to use objects in a pool
+*/
 public interface Poolable {
 
     /** Resets the object for reuse. Object references should be nulled and fields may be set to default values. */
